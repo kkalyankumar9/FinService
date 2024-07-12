b9c1ebb91159551191f17dadd723eeeaeb181f4e
kkalyankumar9 <kummarikalyan010@gmail.com>
2024-07-12 07:58:04 +0530

    cors issue

--- server/src/Models/userAuthModel.js
+++ server/src/Models/userAuthModel.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: {
-        type: String
+        type: String,
+        required: true, 
+
     },
     email: {
         type: String,
